6765c17c5ba86919a22f3de981a91b8a69145a1e
Lorissa <lorissa.becker@gmail.com>
2024-04-06 15:41:25 -0400

    Starting script

--- include/strings.h
+++ include/strings.h
@@ -345,6 +345,7 @@ extern const u8 gText_Birch_BoyOrGirl[];
 extern const u8 gText_Birch_WhatsYourName[];
 extern const u8 gText_Birch_SoItsPlayer[];
 extern const u8 gText_Birch_YourePlayer[];
+extern const u8 gText_Birch_SetTime[];
 extern const u8 gText_Birch_AreYouReady[];
 extern const u8 gText_ContinueMenuPlayer[];
 extern const u8 gText_ContinueMenuTime[];
